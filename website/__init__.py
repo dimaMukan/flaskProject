aa183de2383977887d7cbd21fc634fae12e51df2
dimaMukan <ds.mukan@gmail.com>
2023-09-22 22:40:07 +0100

    models

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from .views import views
+from .auth import auth
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'a'
-    from .views import views
-    from .auth import auth
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
 
     app.register_blueprint(views,url_prefix='/')
     app.register_blueprint(auth,url_prefix='/')
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,39 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,flash
 
 auth = Blueprint('auth',__name__)
 
 @auth.route('/login',methods=["GET","Post"])
 def log_in():
+    data = request.form
+    print(data)
     return render_template("login.html", text='text',user='Tim',boolean=True)
 
 @auth.route('/logout')
 def log_out():
     return render_template("home.html")
 
-@auth.route('sign-up')
+@auth.route('sign-up',methods=["GET","Post"])
 def sign_up():
-    return render_template("sign_up.html",methods=["GET","Post"])
+    if request.method == 'POST':
+        data = request.form
+        print(data)
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email)<4:
+            flash('Email must be more than 4 characters', category='Error')
+        elif len(first_name)<2:
+            flash('First name must be more than 1 characters', category='Error')
+        elif len(password1)<7:
+            flash('Password must be more than 6 characters', category='Error')
+        elif password1 != password2:
+            flash('Passwords don`t match', category='Error')
+        else:
+            flash('Account created!',category='Success')
+
+
+
+
+    return render_template("sign_up.html")
