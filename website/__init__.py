0ce62237286ebe3ee6186ded48d6e28e5b31f786
dimaMukan <ds.mukan@gmail.com>
2023-09-23 13:15:13 +0100

    Added db

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,17 @@
-from .views import views
-from .auth import auth
-from .models import User, Notes
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
-from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
+
+    from .views import views
+    from .auth import auth
+    from .models import User, Notes
+
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'a'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
@@ -19,12 +21,19 @@ def create_app():
     app.register_blueprint(views,url_prefix='/')
     app.register_blueprint(auth,url_prefix='/')
 
-    create_db(app)
+    with app.app_context():
+        db.create_all()
 
     return app
 
 
 def create_db(app):
-    if not path.exists('website/'+ DB_NAME):
+    if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
-        print('Created DB')
+        print('Created Database!')
+
+
+
+
+
+
